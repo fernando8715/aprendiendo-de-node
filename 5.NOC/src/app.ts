7893bcc74ad849ab5a54a2192d22104fd30520bc
Fernando Ruiz <fernando8715@hotmail.com>
2026-01-03 22:56:29 -0500

    configurando MongoDB con moongose

--- 5.NOC/src/app.ts
+++ 5.NOC/src/app.ts
@@ -1,14 +1,19 @@
 import { ServerApp } from '../src/presentation/Server';
 import { envs } from './config/plugins/envs.plugins';
+import { MongoDatabase } from './data/mongo';
 
 (() => {
     main();
 })();
 
 
-function main() {
+async function main() {
 
     // console.log(envs);
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    });
 
-    ServerApp.start();
+    // ServerApp.start();
 }
